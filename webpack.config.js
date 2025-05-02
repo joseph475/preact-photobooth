06a4c3d0da4eea3fb92d12e20dd32d2270b55c26
joseph475 <markjoseph475@gmail.com>
2025-05-03 05:32:46 +0800

    fix deployment issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -43,6 +44,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' }
+      ],
     }),
   ],
   devServer: {
